b1f4e3ae3a721cebdadda39de5b0a35c0b0f02c8
KaminatorJoula <124656803+KaminatorJoula@users.noreply.github.com>
2023-03-02 13:15:14 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const button = document.querySelector(".boton-especial");
+
+function mensaje() {
+    alert("Se viene pagina nop0r");
+    return 0;
+}
+
+button.addEventListener("click", mensaje);
