312c3ba932b0d152680eef47aaeaaf82ee815a33
KaminatorJoula <124656803+KaminatorJoula@users.noreply.github.com>
2023-04-19 00:10:35 -0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const button = document.querySelector(".boton-especial");
-
-function mensaje() {
-    alert("Se viene pagina nop0r");
-    return 0;
-}
-
-button.addEventListener("click", mensaje);
